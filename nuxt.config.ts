b971d0a81925a051f22d56f8c02ef816eb152b65
alice-x <hi.burnout@gmail.com>
2026-05-18 14:14:58 +0900

    basic-auth修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     notionToken: process.env.NOTION_TOKEN,
     notionDatabaseId: process.env.NOTION_DATABASE_ID,
-    translateApiUrl: process.env.TRANSLATE_API_URL
+    translateApiUrl: process.env.TRANSLATE_API_URL,
+    basicAuthUser: process.env.BASIC_AUTH_USER,
+    basicAuthPassword: process.env.BASIC_AUTH_PASSWORD
   },
 
   modules: ["@nuxtjs/tailwindcss"]
